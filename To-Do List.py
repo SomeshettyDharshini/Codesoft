ac025bd448d8effadecba6063e48c08661d1009f
Someshetty.Dharshini <someshettydharshini@gmail.com>
2025-04-30 16:54:47 +0530

    To-Do List.py

--- To-Do List.py
+++ To-Do List.py
@@ -1,33 +1,34 @@
 tasks=[]
-def add_task(tasks):
+#Function Creation :Reuse the block of code ,by calling a function.
+def add_task(tasks):         #Function for Adding Tasks
     task=input("Add Task into To-DO List:")
     tasks.append(task)
     print(f"'{task}'added Successfully")
-def view_task(tasks):
+def view_task(tasks):         #Function For Viewing Tasks
     print(f"'{tasks}' your Tasks")
-def update_task(tasks):
+def update_task(tasks):      #Function Updating Tasks
     view_task(tasks)
     task_id=int(input("Enter the Number the Task to be Updated:"))  
-    if 0<= task_id<len(tasks):
+    if 0<= task_id<len(tasks):   #If Statement 
        new_task=input("Enter the new task:")
        tasks[task_id]=new_task
        print(f"'{new_task}' is updated Successfully")
     view_task(tasks)   
-def delete_task(tasks):
+def delete_task(tasks):    #Function For Deleting Tasks
      view_task(tasks)
      task_id=int(input("Enter the Number the Task to be deleted:"))
      if 0<= task_id<len(tasks):
          deleted_task=tasks.pop(task_id)
          print(f"'{deleted_task}' is deleted successfully")
      view_task(tasks)        
-while True:
+while True:      #While Loop: While loop runs as long as a condition is True.
     print("To-do List Tasks: ")
     print("1.Add Task into List. ")
     print("2.View Task in List. ")
     print("3.Update Task in List. ")
     print("4.Delete Task from List. ")
     choice=int(input("Enter your Choice:"))
-         
+    #Conditional statement (elif): If one condition is not satisfied ,it checks another elif sattement ,if not prints else statement.
     if choice==1:
       add_task(tasks)
     elif choice==2:
