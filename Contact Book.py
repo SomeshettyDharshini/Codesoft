22935f2b09dcb253688d2d72b49a64b128fd4124
Someshetty.Dharshini <someshettydharshini@gmail.com>
2025-04-30 16:39:14 +0530

    Contact Book.py

--- Contact Book.py
+++ Contact Book.py
@@ -1,24 +1,24 @@
-contact_book = {}
+contact_book = {}     
 
-def add_contact():
-  name = input("Enter contact name: ")
+def add_contact():              # Function Creation(For Adding contacts)
+  name = input("Enter contact name: ")      # Taking input from Users    
   phone = input("Enter phone number:+91 ")
   email = input("Enter email: ")
-  contact_book[name] = {"phone": phone, "email": email}
-
-def search_contact():
+  contact_book[name] = {"phone": phone, "email": email}  # To Store the input's of user in a varaiable
+ 
+def search_contact():      # Function Creation (For Searching Contacts)
   search_term = input("Enter name to search: ")
-  for name, details in contact_book.items():
-    if search_term in name:
+  for name, details in contact_book.items():     #For loop:Its allows to repeat a block of code multiple times.   
+    if search_term in name:            #Conditional Statement(If): If condition is True prints If Statement otherwise it prints else statement.
       print(f"Name: {name}, Phone: {details['phone']}, Email: {details['email']}")
     else:
       print("Name doesn't Found")
-def display_contact():
+def display_contact():      # Function Creation (For Displaying contacts)
    print("\nAll Contacts:")
    for name,details in contact_book.items():
        print(f"Name: {name}, Phone: {details['phone']}, Email: {details['email']}")
            
-while True:
+while True:        # While Loop: The While Loop runs as Long as a condition is satisfied.
   print("\nContact Book Menu:")
   print("1. Add Contact")
   print("2. Search Contact")
@@ -26,7 +26,7 @@ while True:
   
   choice = input("Enter your choice: ")
 
-  if choice == "1":
+  if choice == "1":            #
     add_contact()
   elif choice == "2":
     search_contact()
